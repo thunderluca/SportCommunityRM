c854c6fa7292d5705349b4ed530d8402a4baa625
Luca Montanari <luca.montanari@selecta.it>
2017-09-26 20:09:42 +0200

    Fixed responsive images

--- src/SportCommunityRM.WebSite/ViewModels/User/DetailViewModel.cs
+++ src/SportCommunityRM.WebSite/ViewModels/User/DetailViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,15 +10,18 @@ namespace SportCommunityRM.WebSite.ViewModels.User
     {
         public Guid Id { get; set; }
 
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
+
+        [Display(Name = "Birth date")]
+        public DateTime BirthDate { get; set; }
 
         public string PictureUrl { get; set; }
 
         public string Name => $"{FirstName} {LastName}";
-
-        public DateTime BirthDate { get; set; }
 
         public IEnumerable<Team> Teams { get; set; }
 
